f674b316875ce6cc36a4199053e0d6ba06cadbd2
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-03-13 12:45:06 -0700

    <comment>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,26 +14,31 @@ var computerPicks = compLetters[Math.floor(Math.random() * compLetters.length)];
 function reset() {
     GuessesLeft = 10;
     guesses = [];
-    compLetters;
+    computerPicks = compLetters[Math.floor(Math.random() * compLetters.length)];
 }; 
+
 
 // Letters the user can enter
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
 
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
 
     console.log(userGuess);
+    console.log(computerPicks);
+    
     
     if (letters.indexOf(userGuess) > -1) {
 
         // If the userGuess matches the Computer: +1 win, Still 10 guesses
         if (userGuess === computerPicks) {
             wins++;
+            document.getElementById("computer").textContent = computerPicks;
             document.getElementById("wins2").textContent = wins;
-            reset();
-        }
+           reset();
+        } 
 
         // If they choose incorrectly decrement -1 from guesses left
         else if (userGuess != computerPicks) {
