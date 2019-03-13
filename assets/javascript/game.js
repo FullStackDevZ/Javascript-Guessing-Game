dcdaa2ec82cf543d309a765d5a65d60b8e97c6fb
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-03-12 23:15:45 -0700

    <comment>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,35 +10,15 @@ var compLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 var computerPicks = compLetters[Math.floor(Math.random() * compLetters.length)];
 
 
-// Update guesses left
-function GuessesLeftMSG() {
-    document.getElementById("guesses-left").textContent;
-    
-};
-
-// What the user has guessed so far
-    // Separates the guesses via commas
-    // = guesses.join(", ");
-    // YourGuessesMSG.textContent = "Your guesses" + userGuess;
-
-
-// Update wins
-
-
-// Update losses
-
 // print out in reset
 function reset() {
-    guessesLeft = 10;
+    GuessesLeft = 10;
     guesses = [];
-    wins = 0;
-    losses = 0;
+    compLetters;
 }; 
 
-// computerPicks();
-
+// Letters the user can enter
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-// updateGuessesLeft();
 
 document.onkeyup = function (event) {
 
@@ -56,24 +36,21 @@ document.onkeyup = function (event) {
         }
 
         // If they choose incorrectly decrement -1 from guesses left
-        // and +1 to losses
         else if (userGuess != computerPicks) {
             GuessesLeft--;
             guesses.push(userGuess); //adds news element to userGuess
             document.getElementById("guesses-left").textContent = GuessesLeft;
             document.getElementById("guesses-so-far").textContent = guesses;
-        
-            // document.getElementById("losses2").textContent = losses;
-            // lossesMSG();
-            // GuessesLeftMSG();
         }
 
         //Once the # of guesses reaches 0, restart the game
         else if (GuessesLeft === 0) {
             losses++;
-            // document.getElementById("guesses-left").textContent = GuessesLeft;
+            document.getElementById("guesses-left").textContent = GuessesLeft;
+            document.getElementById("guesses-so-far").textContent = guesses;
             reset();
         }
+
     }
 
     
